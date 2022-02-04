488fe61f4d55e19a9a0185c8bc1458ac2644d282
michelypsouza <michely.psouza@gmail.com>
2022-02-04 02:04:03 -0300

    aula 3 do modulo Vetores, Arrays e Listas

---
+++ src/com/loiane/estruturadados/vetor/teste/Aula3.java
@@ -0,0 +1,27 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula3 {
+
+    public static void main(String[] args) {
+
+        //Vetor vetor = new Vetor(5);
+        Vetor vetor = new Vetor(2);
+
+        /*
+        try {
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+            vetor.adiciona("elemento 3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+    }
+}
