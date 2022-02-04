277b0b70d19879047a9bff6c4dc3305c2d4ec1c3
michelypsouza <michely.psouza@gmail.com>
2022-02-04 01:21:14 -0300

    aula 2 do modulo Vetores, Arrays e Listas

---
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -0,0 +1,13 @@
+package com.loiane.estruturadados.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+
+        this.elementos = new String[capacidade];
+
+    }
+
+}
