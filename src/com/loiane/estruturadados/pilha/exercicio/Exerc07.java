1ae27d56358ea75bcf23ec86b6d60573eec9c33d
michelypsouza <michely.psouza@gmail.com>
2022-02-07 05:29:44 -0300

    exercicio 7 do modulo Pilhas

---
+++ src/com/loiane/estruturadados/pilha/exercicio/Exerc07.java
@@ -0,0 +1,39 @@
+package com.loiane.estruturadados.pilha.exercicio;
+
+import java.util.Stack;
+
+public class Exerc07 {
+
+    public static void main(String[] args) {
+
+        imprime(2);
+        imprime(4);
+        imprime(10);
+        imprime(25);
+        imprime(10035);
+
+    }
+
+    public static void imprime(int numero){
+        System.out.println(numero + " em binário é: " + decimalBinario(numero));
+    }
+
+    public static String decimalBinario(int numero){
+
+        Stack<Integer> stack = new Stack<>();
+        String numeroBinario = "";
+        int restoDivisaoBinario;
+
+        while (numero > 0){
+            restoDivisaoBinario = numero % 2;
+            stack.push(restoDivisaoBinario);
+            numero = numero /2;
+        }
+
+        do {
+            numeroBinario += stack.pop();
+        } while (!stack.isEmpty());
+        return numeroBinario;
+    }
+
+}
