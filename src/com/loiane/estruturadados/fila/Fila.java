845c6fafba4059eaea41a0f0504d10502ff4cc10
michelypsouza <michely.psouza@gmail.com>
2022-02-07 06:29:42 -0300

    aula 1 do modulo Filas

---
+++ src/com/loiane/estruturadados/fila/Fila.java
@@ -0,0 +1,17 @@
+package com.loiane.estruturadados.fila;
+
+import com.loiane.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    public Fila() {
+        super();
+    }
+
+
+
+}
