bcb2093a25e5648ef8e6265a4053b87c62d6948b
michelypsouza <michely.psouza@gmail.com>
2022-02-11 00:55:46 -0300

    comentando o exercicio 4 do modulo Filas - atividade complementar nao esta no curso

--- src/com/loiane/estruturadados/fila/exercicio/Exer04.java
+++ src/com/loiane/estruturadados/fila/exercicio/Exer04.java
@@ -8,28 +8,40 @@ public class Exer04 {
 
     public static void main(String[] args) {
 
+        //criando fila para controlar a brincadeira da batata-quente
         Fila<Integer> fila = new Fila<>();
 
+        //definindo a quantidade de criancas participantes da brincadeira
         for (int qtdeCriancas = 0; qtdeCriancas < 10; qtdeCriancas++) {
+            //enfileirando as 10 criancas
             fila.enfileira(qtdeCriancas);
         }
 
+        //escolhendo a crianca que sai da brincadeira com numero aleatorio de acordo com janela de 10 criancas
         Random aleatorio = new Random();
         int numero = 0;
         while (numero == 0) {
-            numero = aleatorio.nextInt(10); //numero = 2 valor mostrado no exercicio na aula
+            numero = aleatorio.nextInt(10); //numero = 2 valor mostrado no exercicio na aula por video
         }
 
+        //a crianca eliminada aleatoriamente sai da rodada
         System.out.println("Número aleatório =  " + numero);
 
         //emular uma fila circular
         while(fila.tamanho() > 1) { // deixa uma pessoa sobrando no final da brincadeira batata quente
             for (int i = 0; i < numero; i++) {
+                /*
+                  O Número aleatorio define o intervalo de eliminacao, ao remover determinado elemento (desenfileirar
+                  o elemento da fila, é necessário enfileira os elementos antecessores ao Eliminado, sendo assim os
+                  elementos vao para o final da fila a cada iteracao)
+                 */
                 fila.enfileira(fila.desenfileira());
             }
+            //desenfileirando todos os elementos, exceto o ultimo
             System.out.println("Eliminado = "+fila.desenfileira());
         }
 
+        //o ultimo eh o vencedor
         System.out.println("Ganhador: " + fila.desenfileira());
 
     }
